86965a69e930b2b4276ba7042ce46108d9a983f1
Jake <jakerfilut@gmail.com>
2022-04-10 22:41:45 -0600

    Fixed blank cards

--- src/components/List.js
+++ src/components/List.js
@@ -8,32 +8,26 @@ function List({ place }) {
     img = `https://cdn.otstatic.com/legacy-cw/default2-original.png`;
   }
 
-  return (
-    // <div>
-    //   <img src={img} alt={place.name} width="500" height="600" />
-    //   <div>{place.name}</div>
-    //   <div>{place.rating}</div>
-    //   <div>{place.ranking}</div>
-    //   <div>{place.description}</div>
-    // </div>
-
-    <div class="card mb-3">
-      <div class="row no-gutters">
-        <div class="col-md-4">
-          <img src={img} class="card-img" alt={place.name} />
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">{place.name}</h5>
-            <p class="card-text">{place.description}</p>
-            <p class="card-text">
-              <small class="text-muted">{place.ranking}</small>
-            </p>
+  if (place.name) {
+    return (
+      <div class="card mb-3">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src={img} class="card-img" alt={place.name} />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{place.name}</h5>
+              <p class="card-text">{place.description}</p>
+              <p class="card-text">
+                <small class="text-muted">{place.ranking}</small>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default List;
